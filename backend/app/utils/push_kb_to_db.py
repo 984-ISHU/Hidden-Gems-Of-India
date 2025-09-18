5cc37e80c175a8b534ff76c4cd91d53982209f5e
Mohammed Saqlain <saqlain2204@gmail.com>
2025-09-18 21:54:38 +0530

    Fixed bugs

--- backend/app/utils/push_kb_to_db.py
+++ backend/app/utils/push_kb_to_db.py
@@ -27,7 +27,20 @@ def load_json_to_mongo():
     print(f"Inserted {len(result.inserted_ids)} documents into '{DB_NAME}.{COLLECTION_NAME}'")
     
 if __name__ == "__main__":
-    JSON_FILE_PATH = "../../notebooks/output_embeddings.json"  
+    JSON_FILE_PATH = "../../notebooks/output_embeddings_list.json"  
     load_dotenv()
     
-    load_json_to_mongo()    
+    load_json_to_mongo()   
+    # import json
+
+    # with open("../../notebooks/output_embeddings.json", "r", encoding="utf-8") as f:
+    #     data = json.load(f)
+
+    # # If data is a dict, convert to list of values
+    # if isinstance(data, dict):
+    #     data = list(data.values())
+
+    # with open("output_embeddings_list.json", "w", encoding="utf-8") as f:
+    #     json.dump(data, f, indent=2, ensure_ascii=False)
+
+    # print("Converted to list of JSON objects in output_embeddings_list.json") 
